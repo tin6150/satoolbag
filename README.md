759f9d6de0795df986103b6ab52926a2f728e29b
tin6150 <tin6150+LL486@gmail.com>
2024-05-02 15:46:11 -0700

    empower sys admin :)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # satoolbag
 sys admin tool bag - ie fat container with many sys admin programs in it
+
+
+eg text based web browser like elinks, lynx
+
---
+++ install_dependencies.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+## install python packages and add libraries
+
+date
+apt-get update
+# text based web browser
+apt-get -y install elinks elinks-data elinks-doc lynx  wget curl
+apt-get -y install ldap-utils ldapvi   ldb-tools 
+apt-get -y install firefox xterm
+
+# TBD smc, dell ipmitool...
+# wget to them maybe behind login requirements
+
+echo "==================================="
+
+# perhaps tmp only, atlas need to invoke docker, so this maybe helpful
+#xx old do not use  apt-get -y install docker-compose
+# https://docs.docker.com/engine/install/ubuntu/
+# Add Docker's official GPG key:
+apt-get -y install ca-certificates 
+install -m 0755 -d /etc/apt/keyrings
+
+# Add the repository to Apt sources:
+apt-get update
+
+echo "==================================="
+echo "==================================="
+
+
+# cd /
