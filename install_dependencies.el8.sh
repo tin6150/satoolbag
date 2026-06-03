c48cb8c94d511c1ad6cc6baf0920f4664676158d
tin6150 <sn+newbox@grumpyxmas.com>
2026-06-02 17:46:07 -0700

    el8: mft/mst deps, add usbutils, systemd-udev.  weasel

--- install_dependencies.el8.sh
+++ install_dependencies.el8.sh
@@ -9,6 +9,8 @@ yum install -y curl   # seems installed by here
 yum install -y wget 
 yum install -y findutils     # needed by mellanox mdt/mst
 yum install -y pciutils      # needed by mellanox mdt/mst   find, xargs
+yum install -y systemd-udev  # needed by mellanox mdt/mst   
+yum install -y usbutils      # needed by mellanox mdt/mst   
 yum install -y epel-release
 yum install -y ldap-utils ldapvi   ldb-tools 
 yum install -y firefox xterm xauth
